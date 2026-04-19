6737d44895cfb093d4b64339d9af1548ad99e0a1
Yanuxynovel <qianjayadharma@gmail.com>
2026-04-20 06:21:38 +0700

    Create novel-1.js

---
+++ buku/novel-1.js
@@ -0,0 +1,4 @@
+window.currentBookContent = `SISTEM KEKAYAAN RAHASIA
+
+[BAB 1: AWAL MULA]
+Kim Do-yun adalah pemuda yang tumbuh dalam keluarga sederhana... (Lanjutkan paste naskah dari Word Anda)`;
